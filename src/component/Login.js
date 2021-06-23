3e4ca6fa06893eaf6defdebb9496798bc94007fe
Bhoft <hoft@eurescom.eu>
2021-06-23 12:00:55 +0200

    use serverUri from config file for login button urls

--- src/component/Login.js
+++ src/component/Login.js
@@ -13,10 +13,9 @@ export default function Login(props) {
             <div className="jumbotron">
                 <h1 className="display-4">Welcome to Eureka Clusters Backend</h1>
             </div>
-
-            <a className="btn btn-primary btn-lg" href="https://dev.backend.eureka-clusters.eu/oauth2/login/via/itea.html">Login via ITEA Office</a>
-            <a className="btn btn-primary btn-lg" href="https://dev.backend.eureka-clusters.eu/oauth2/login/via/celtic.html">Login via Celtic</a>
-            <a className="btn btn-primary btn-lg" href="https://dev.backend.eureka-clusters.eu/oauth2/login/via/penta.html">Login via Penta-Euripides</a>
+            <a className="btn btn-primary btn-lg" href={serverUri+'/oauth2/login/via/itea.html'}>Login via ITEA Office</a>
+            <a className="btn btn-primary btn-lg" href={serverUri+'/oauth2/login/via/celtic.html'}>Login via Celtic</a>
+            <a className="btn btn-primary btn-lg" href={serverUri+'/oauth2/login/via/penta.html'}>Login via Penta-Euripides</a>
         </div>
     );
 }
