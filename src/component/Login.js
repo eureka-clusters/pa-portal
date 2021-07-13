477a25d09fab60ab4f8044bc56f94256e84e97e5
Bhoft <hoft@eurescom.eu>
2021-07-13 10:39:39 +0200

    re-added dynamic login urls (otherwise i can't use my local backend)

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import Config from "../constants/Config";
 
 export default function Login(props) {
+
+    const serverUri = Config.SERVER_URI;
 
     return (
         <div className="container">
             <div className="jumbotron">
                 <h1 className="display-4">Welcome to Eureka Clusters Backend</h1>
             </div>
-
-            <a className="btn btn-primary btn-lg" href="https://dev.backend.eureka-clusters.eu/oauth2/login/via/itea.html">Login via ITEA Office</a>
-            <a className="btn btn-primary btn-lg" href="https://dev.backend.eureka-clusters.eu/oauth2/login/via/celtic.html">Login via Celtic</a>
-            <a className="btn btn-primary btn-lg" href="https://dev.backend.eureka-clusters.eu/oauth2/login/via/penta.html">Login via Penta-Euripides</a>
+            <a className="btn btn-primary btn-lg" href={serverUri+'/oauth2/login/via/itea.html'}>Login via ITEA Office</a>
+            <a className="btn btn-primary btn-lg" href={serverUri+'/oauth2/login/via/celtic.html'}>Login via Celtic</a>
+            <a className="btn btn-primary btn-lg" href={serverUri+'/oauth2/login/via/penta.html'}>Login via Penta-Euripides</a>
         </div>
     );
 }
