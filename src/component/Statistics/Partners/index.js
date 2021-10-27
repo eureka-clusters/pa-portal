51ae41b0ee0d489450dd49ba09610c92db54a5fb
Bhoft <hoft@eurescom.eu>
2021-10-27 10:09:36 +0200

    fixed the statistics partner filtering and rendering of the facets

--- src/component/Statistics/Partners/index.js
+++ src/component/Statistics/Partners/index.js
@@ -1,28 +1,58 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
+import useState from 'react-usestateref';
 import { Form, Button } from "react-bootstrap";
 
 import PartnerTable from "./partner-table";
 import PartnerFacets from './partner-facets';
+import { getFilter } from '../../../function/Api';
 
 // import ResultChart from "./ResultChart";
 // import downloadBase64File from "../../function/DownloadBase64";
 
 export default function PartnerStatistics(props) {
 
-    //const [hash, setHash] = useState(true);
-
-    const [filter, setFilter] = useState({
+    const defaultFilter = {
         country: [],
-        country_method: 'or',
+        country_method: 'or',            // @johan are these required on the backend? as "and" makes no sence here
         organisation_type: [],
-        organisation_type_method: 'or',
+        organisation_type_method: 'or',  // @johan are these required on the backend? as "and" makes no sence here
         project_status: [],
-        project_status_method: 'or',
+        project_status_method: 'or',     // @johan are these required on the backend? as "and" makes no sence here
         primary_cluster: [],
-        primary_cluster_method: 'or',
+        primary_cluster_method: 'or',    // @johan are these required on the backend? as "and" makes no sence here
         year: [],
-    });
+    };
 
+    const getFilterFromHash = (setFilterMethod, useAsFilter = false) => {
+        if (props.location.hash) {
+            var hash = atob(props.location.hash.substring(1));
+            var newFilter = JSON.parse(hash);
+            console.log(['filter from hash', newFilter]);
+            if (useAsFilter && typeof setFilterMethod == "function") {
+                // console.log('filter set');
+                setFilterMethod(prevState => ({
+                    ...prevState, ...newFilter
+                }))
+            }
+            return newFilter;
+        } else {
+            // required to set the filter if browser back button changes url to /projects without any hash
+            if (useAsFilter && typeof setFilterMethod == "function") {
+                // set default filter when no hash is given
+                setFilterMethod(prevState => ({
+                    ...prevState, ...defaultFilter
+                }))
+            }
+        }
+    }
+
+    // returns the default filter merged with the filters set via the hash filter values
+    const getDefaultFilter = () => {
+        let merged = { ...defaultFilter, ...getFilterFromHash() };
+        return merged;
+    }
+
+    const [filter, setFilter, filter_ref] = useState(() => getDefaultFilter());
 
     const downloadExcel = () => {
         // fetch(serverUri + '/api/statistics/download/' + output + '/' + btoa(JSON.stringify(filter)),
@@ -40,6 +70,13 @@ export default function PartnerStatistics(props) {
         //     downloadBase64File(mimetype, res.download, 'Download' + extension);
         // });
     }
+
+    // update the filter depending on the hash in the url
+    useEffect(() => {
+        getFilterFromHash(setFilter, true);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [props.location.hash]);
+
 
     const updateFilter = (event) => {
         const target = event.target;
@@ -69,14 +106,15 @@ export default function PartnerStatistics(props) {
         updateHash();
     }
 
+    const updateResults = () => { }
 
     const updateHash = () => {
+        var hash = getFilter(filter_ref.current);
         props.history.push({
-            'hash': btoa(JSON.stringify(filter))
+            'hash': hash
         });
-
-        //setHash(btoa(JSON.stringify(filter)))
     }
+    
 
     return (
         <React.Fragment>
@@ -88,7 +126,7 @@ export default function PartnerStatistics(props) {
                 </div>
                 <div className={'row'}>
                     <div className={'col-2'}>
-                        <PartnerFacets filter={filter} updateFilter={updateFilter} updateHash={updateHash} />
+                        <PartnerFacets filter={filter} setFilter={setFilter} updateFilter={updateFilter} updateHash={updateHash} updateResults={updateResults} />
                     </div>
                     <div className={'col-10'}>
 
--- src/component/Statistics/Partners/partner-facets.js
+++ src/component/Statistics/Partners/partner-facets.js
@@ -1,11 +1,26 @@
 import React from 'react';
 
 import { Form } from "react-bootstrap";
+import BootstrapSwitchButton from "bootstrap-switch-button-react";
 import { apiStates, Api, getFilter, ApiError } from '../../../function/Api';
 
-const PartnerFacets = ({ filter, updateFilter, updateHash }) => {
+const PartnerFacets = ({ filter, setFilter, updateFilter, updateResults, updateHash }) => {
 
     const { state, error, data } = Api('/statistics/facets/partner?filter=' + getFilter(filter));
+
+    const updateOrganisationTypeMethod = (event) => {
+        // filter['organisation_type_method'] = event ? 'and' : 'or';
+        var updatedValues = {
+            organisation_type_method: (event ? 'and' : 'or')
+        };
+        setFilter(prevState => ({
+            ...prevState, ...updatedValues
+        }))
+
+        updateResults();
+        updateHash();
+    }
+
 
     switch (state) {
         case apiStates.ERROR:
@@ -16,14 +31,21 @@ const PartnerFacets = ({ filter, updateFilter, updateHash }) => {
 
             return (
                 <>
+                    <pre className='debug'>{JSON.stringify(filter, undefined, 2)}</pre>
                     <fieldset>
                         <legend><small>Countries</small></legend>
-        
+                       
                         {facetData[0] && facetData[0].map((country, i) => (
                             <div key={i}>
                                 <Form.Check type={'checkbox'} id={`check-country-${i}`}>
-                                    <Form.Check.Input name="country" value={country['country']}
-                                        onChange={updateFilter} />
+                                    <Form.Check.Input
+                                        name="country"
+                                        value={country['country']}
+                                        onChange={updateFilter}
+                                        checked={
+                                            filter['country'].indexOf(country['country']) > -1
+                                        }
+                                    />
                                     <Form.Check.Label>{country['country']} ({country['amount']})</Form.Check.Label>
                                 </Form.Check>
                             </div>
@@ -33,12 +55,18 @@ const PartnerFacets = ({ filter, updateFilter, updateHash }) => {
                     <fieldset>
                         <legend><small>Organisation type</small></legend>
                
-                        {facetData[1] && facetData[1].map((organisationType, i) => (
+                        {facetData[1] && facetData[1].map((partnerType, i) => (
                             <div key={i}>
                                 <Form.Check type={'checkbox'} id={`check-type-${i}`}>
-                                    <Form.Check.Input name="organisation_type" value={organisationType['organisationType']}
-                                        onChange={updateFilter} />
-                                    <Form.Check.Label>{organisationType['organisationType']} ({organisationType['amount']})</Form.Check.Label>
+                                    <Form.Check.Input
+                                        name="organisation_type"
+                                        value={partnerType['organisationType']}
+                                        onChange={updateFilter}
+                                        checked={
+                                            filter['organisation_type'].indexOf(partnerType['organisationType']) > -1
+                                        }
+                                    />
+                                    <Form.Check.Label>{partnerType['organisationType']} ({partnerType['amount']})</Form.Check.Label>
                                 </Form.Check>
                             </div>
                         ))}
@@ -46,12 +74,18 @@ const PartnerFacets = ({ filter, updateFilter, updateHash }) => {
         
                     <fieldset>
                         <legend><small>Project Status</small></legend>
-        
+
                         {facetData[2] && facetData[2].map((projectStatus, i) => (
                             <div key={i}>
                                 <Form.Check type={'checkbox'} id={`check-project-status-${i}`}>
-                                    <Form.Check.Input name="project_status" value={projectStatus['projectStatus']}
-                                        onChange={updateFilter} />
+                                    <Form.Check.Input
+                                        name="project_status"
+                                        value={projectStatus['projectStatus']}
+                                        onChange={updateFilter}
+                                        checked={
+                                            filter['project_status'].indexOf(projectStatus['projectStatus']) > -1
+                                        }
+                                    />
                                     <Form.Check.Label>{projectStatus['projectStatus']} ({projectStatus['amount']})</Form.Check.Label>
                                 </Form.Check>
                             </div>
@@ -64,8 +98,14 @@ const PartnerFacets = ({ filter, updateFilter, updateHash }) => {
                         {facetData[3] && facetData[3].map((primaryCluster, i) => (
                             <div key={i}>
                                 <Form.Check type={'checkbox'} id={`check-primary-cluster-${i}`}>
-                                    <Form.Check.Input name="primary_cluster" value={primaryCluster['primaryCluster']}
-                                        onChange={updateFilter} />
+                                    <Form.Check.Input
+                                        name="primary_cluster"
+                                        value={primaryCluster['primaryCluster']}
+                                        onChange={updateFilter}
+                                        checked={
+                                            filter['primary_cluster'].indexOf(primaryCluster['primaryCluster']) > -1
+                                        }
+                                    />
                                     <Form.Check.Label>{primaryCluster['primaryCluster']} ({primaryCluster['amount']})</Form.Check.Label>
                                 </Form.Check>
                             </div>
@@ -74,13 +114,21 @@ const PartnerFacets = ({ filter, updateFilter, updateHash }) => {
         
                     <fieldset>
                         <legend><small>Years</small></legend>
-        
+                        
                         {facetData[4] && facetData[4].map((year, i) => (
                             <div key={i}>
                                 <Form.Check type={'checkbox'} id={`check-year-${i}`}>
-                                    <Form.Check.Input name="year" value={year['year']}
-                                        onChange={updateFilter} />
+                                    <Form.Check.Input
+                                        name="primary_cluster"
+                                        value={year['year']}
+                                        onChange={updateFilter}
+                                        checked={
+                                            filter['year'].indexOf(year['year']) > -1
+                                        }
+                                    />
                                     <Form.Check.Label>{year['year']}</Form.Check.Label>
+                                    {/* @johan do we add an amount for year?
+                                     <Form.Check.Label>{year['year']} ({year['amount']})</Form.Check.Label> */}
                                 </Form.Check>
                             </div>
                         ))}
