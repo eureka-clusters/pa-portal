98ee9f2423f67b34d1381083d5d8158cdf25d339
Bhoft <hoft@eurescom.eu>
2021-10-27 16:05:49 +0200

    WIP: First test of a TableFilter Component used in the /statistics/projects page

--- src/component/Statistics/Projects/index.js
+++ src/component/Statistics/Projects/index.js
@@ -5,7 +5,12 @@ import useState from 'react-usestateref';
 import { Form, Button } from "react-bootstrap";
 import ProjectTable from "./project-table";
 import ProjectFacets from './project-facets';
-import { getFilter } from '../../../function/Api';
+import { getFilter, 
+/*getFilterFromHash, updateHash, updateFilter*/
+} from '../../../function/Api';
+
+
+import TableFilter from '../../../function/Api/TableFilter';
 
 
 export default function ProjectStatistics(props) {
@@ -21,37 +26,55 @@ export default function ProjectStatistics(props) {
         primary_cluster_method: 'or',
         year: [],
     };
-  
-    const getFilterFromHash = (setFilterMethod, useAsFilter = false) => {
-        if (props.location.hash) {
-            var hash = atob(props.location.hash.substring(1));
-            var newFilter = JSON.parse(hash);
-            console.log(['filter from hash', newFilter]);
-            if (useAsFilter && typeof setFilterMethod == "function") {
-                // console.log('filter set');
-                setFilterMethod(prevState => ({
-                    ...prevState, ...newFilter
-                }))
-            }
-            return newFilter;
-        } else {
-            // required to set the filter if browser back button changes url to /projects without any hash
-            if (useAsFilter && typeof setFilterMethod == "function") {
-                // set default filter when no hash is given
-                setFilterMethod(prevState => ({
-                    ...prevState, ...defaultFilter
-                }))
-            }
-        }
-    }
+
+    const { test, updateHash, updateFilter, filter, setFilter  } = TableFilter({ props, defaultFilter});
+
+    // useEffect(() => {
+    //     console.log(['props', props]);
+    //     console.log(['defaultFilter', defaultFilter]);
+
+    //     // TableFilter(defaultFilter);
+    //     <TableFilter defaultFilter={defaultFilter} />
+
+    // }, [props, defaultFilter]);
+
+    // TableFilter(props, defaultFilter);
+
+    // <TableFilter 
+    //     props = { props }
+    //     defaultFilter = {defaultFilter}
+    // />
+
+    // const getFilterFromHash = (setFilterMethod, useAsFilter = false) => {
+    //     if (props.location.hash) {
+    //         var hash = atob(props.location.hash.substring(1));
+    //         var newFilter = JSON.parse(hash);
+    //         console.log(['filter from hash', newFilter]);
+    //         if (useAsFilter && typeof setFilterMethod == "function") {
+    //             // console.log('filter set');
+    //             setFilterMethod(prevState => ({
+    //                 ...prevState, ...newFilter
+    //             }))
+    //         }
+    //         return newFilter;
+    //     } else {
+    //         // required to set the filter if browser back button changes url to /projects without any hash
+    //         if (useAsFilter && typeof setFilterMethod == "function") {
+    //             // set default filter when no hash is given
+    //             setFilterMethod(prevState => ({
+    //                 ...prevState, ...defaultFilter
+    //             }))
+    //         }
+    //     }
+    // }
     
     // returns the default filter merged with the filters set via the hash filter values
-    const getDefaultFilter = () => {
-        let merged = { ...defaultFilter, ...getFilterFromHash() };
-        return merged;
-    }
+    // const getDefaultFilter = () => {
+    //     let merged = { ...defaultFilter, ...getFilterFromHash() };
+    //     return merged;
+    // }
 
-    const [filter, setFilter, filter_ref] = useState(() => getDefaultFilter());
+    // const [filter, setFilter, filter_ref] = useState(() => getDefaultFilter());
 
     const downloadExcel = () => {
         // fetch(serverUri + '/api/statistics/download/' + output + '/' + btoa(JSON.stringify(filter)),
@@ -70,50 +93,53 @@ export default function ProjectStatistics(props) {
         // });
     }
 
-    // update the filter depending on the hash in the url
-    useEffect(() => {
-        getFilterFromHash(setFilter, true);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [props.location.hash]);
+    // // update the filter depending on the hash in the url
+    // useEffect(() => {
+    //     getFilterFromHash(setFilter, true);
+    //     // eslint-disable-next-line react-hooks/exhaustive-deps
+    // }, [props.location.hash]);
 
 
-    const updateFilter = (event) => {
-        const target = event.target;
-        var name = target.name;
-        var value = target.value;
-        var updatedValues = {};
+    // const updateFilter = (event) => {
+    //     const target = event.target;
+    //     var name = target.name;
+    //     var value = target.value;
+    //     var updatedValues = {};
       
-        if (target.type === 'checkbox') { 
-            // slice is required otherwise currentValue would be reference to filter[name] and any modification will change filter directly
-            var currentValue = filter[name].slice(); 
-            if (target.checked) {
-                currentValue.push(value);
-            } else {
-                const index = currentValue.indexOf(value);
-                currentValue.splice(index, 1);
-            }
-            updatedValues[name] = currentValue;
-        } else {
-            updatedValues[name] = value;
-        }
-        setFilter(prevState => ({
-            ...prevState, ...updatedValues
-        }))
-        updateHash();
-    }
+    //     if (target.type === 'checkbox') { 
+    //         // slice is required otherwise currentValue would be reference to filter[name] and any modification will change filter directly
+    //         var currentValue = filter[name].slice(); 
+    //         if (target.checked) {
+    //             currentValue.push(value);
+    //         } else {
+    //             const index = currentValue.indexOf(value);
+    //             currentValue.splice(index, 1);
+    //         }
+    //         updatedValues[name] = currentValue;
+    //     } else {
+    //         updatedValues[name] = value;
+    //     }
+    //     setFilter(prevState => ({
+    //         ...prevState, ...updatedValues
+    //     }))
+    //     updateHash();
+    // }
 
     const updateResults = () => { }
 
 
-    const updateHash = () => {
-        var hash = getFilter(filter_ref.current);
-        props.history.push({
-            'hash': hash
-        });
-    }
+    // const updateHash = () => {
+    //     var hash = getFilter(filter_ref.current);
+    //     props.history.push({
+    //         'hash': hash
+    //     });
+    // }
    
     return (
         <React.Fragment>
+            
+            {test}
+
             <Form>
                 <div className={'row'}>
                     <div className={'col-12'}>
