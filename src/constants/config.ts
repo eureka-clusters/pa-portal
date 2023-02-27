d73bc0465d59dcfb33cb054e79fc5010a9dea57b
Johan van der Heide <info@jield.nl>
2023-02-27 09:53:13 +0100

    Typo fix

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,5 +1,5 @@
 const config = {
-    SERVER_URI: import.meta.env.VITE_SERVER_URI ?? 'https://api.eurekclusters.eu',
+    SERVER_URI: import.meta.env.VITE_SERVER_URI ?? 'https://api.eurekaclusters.eu',
 }
 
 export default config;
